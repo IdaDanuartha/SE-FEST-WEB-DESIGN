05e0c2d92ac80a83cc31d3c0cf1be7985b63d752
IdaDanuartha <danuart21@gmail.com>
2023-06-04 06:57:46 +0800

    change dummy data in famous figure and add background color to navbar when window scrolled

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,16 @@ let swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
-    },
-  });
+  },
+});
+
+
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY > 50) {
+    navbar.classList.add('active')
+  } else {
+    navbar.classList.remove('active')
+  }
+})
